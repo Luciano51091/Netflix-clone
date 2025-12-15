d066f912df1a4741cc0ed796c839f2b689656c11
Luciano Vaccaro <luciano.vaccaro510@gmail.com>
2025-12-15 01:37:53 +0100

    first commit

---
+++ scrip.js
@@ -0,0 +1,47 @@
+document.getElementById("scrollLeft1").addEventListener("click", function () {
+  const container = document.getElementById("imageContainer1");
+  container.scrollBy({
+    left: -2000,
+    behavior: "smooth",
+  });
+});
+
+document.getElementById("scrollRight1").addEventListener("click", function () {
+  const container = document.getElementById("imageContainer1");
+  container.scrollBy({
+    left: 2000,
+    behavior: "smooth",
+  });
+});
+
+document.getElementById("scrollLeft2").addEventListener("click", function () {
+  const container = document.getElementById("imageContainer2");
+  container.scrollBy({
+    left: -2000,
+    behavior: "smooth",
+  });
+});
+
+document.getElementById("scrollRight2").addEventListener("click", function () {
+  const container = document.getElementById("imageContainer2");
+  container.scrollBy({
+    left: 2000,
+    behavior: "smooth",
+  });
+});
+
+document.getElementById("scrollLeft3").addEventListener("click", function () {
+  const container = document.getElementById("imageContainer3");
+  container.scrollBy({
+    left: -2000,
+    behavior: "smooth",
+  });
+});
+
+document.getElementById("scrollRight3").addEventListener("click", function () {
+  const container = document.getElementById("imageContainer3");
+  container.scrollBy({
+    left: 2000,
+    behavior: "smooth",
+  });
+});
